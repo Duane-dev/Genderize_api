8a070f54d1f0c174860d66911fb618c56ff105e0
Duane-dev <correa1800468@mls.ceu.edu.ph>
2021-09-16 21:21:58 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController;
+use App\Http\Controllers\finalsApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/app/{name}', [ApiController::class, "getName"]);
+Route::get('/app/{name}', [finalsApiController::class, "getName"]);
